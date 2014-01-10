85889a9873cea180963039f435829d8ea9c1b5a4
mediochiquito <mateomenrstrina@MacBook-Pro-de-Mateo.local>
2014-01-10 16:04:55 -0200

    asd

---
+++ js/Facebook.js
@@ -0,0 +1,59 @@
+function Facebook(){
+
+	var self = this;
+	
+	this.init = function(){
+
+		FB.init({
+			  appId: '1435336426698191',
+			  nativeInterface: CDV.FB,
+			  useCachedDialogs: false
+		});
+
+	}
+
+
+	this.conectar = function($callback){
+		
+     	 FB.getLoginStatus(function(response) {
+     	
+	          	if (response.status == 'connected') {
+
+	             		if ( device.platform == 'android' || device.platform == 'Android' ){
+								_uid = response.authResponse.userId;
+								_access_token = response.authResponse.accessToken;
+						}else {
+							 	_uid = response.authResponse.userID;
+							 	_access_token = response.authResponse.accessToken;
+						}
+						$callback();
+
+					} else {
+						
+			             FB.login(function(response2) {
+					 		 // console.log(response2)
+							  if (response2.authResponse) {
+							    	
+							    	if ( device.platform == 'android' || device.platform == 'Android' ){
+											_uid = response2.authResponse.userId;
+											_access_token = response2.authResponse.accessToken;
+									}else {
+										 	_uid = response2.authResponse.userID;
+										 	_access_token = response2.authResponse.accessToken;
+									}
+							    	$callback();
+
+							   } else {
+ 
+							     alert('User cancelled login or did not fully authorize.');
+
+							   }
+						}, {scope: 'email'});
+	         		}
+         });
+
+		
+	}
+
+	
+}
